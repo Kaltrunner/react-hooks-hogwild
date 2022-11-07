81e8061d0e6094f59e25e060a85201d439b9b908
Erik Russell <erikrussells@gmail.com>
2022-11-07 13:55:57 -0500

    commit tree a92fdf5ba371c0055c505b9c2bd247f9863b7c03

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Nav from "./Nav";
-
+import PigTiles from "./PigTiles";
 import hogs from "../porkers_data";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<PigTiles/>
 		</div>
 	);
 }
